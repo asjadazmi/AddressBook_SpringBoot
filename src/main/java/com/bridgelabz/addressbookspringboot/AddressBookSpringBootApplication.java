f40e4fc00b23213fedeffba07c35ec2100621560
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-08-13 18:38:08 +0530

    Added: Welcome to the AddressBook using spring boot

---
+++ src/main/java/com/bridgelabz/addressbookspringboot/AddressBookSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressbookspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookSpringBootApplication.class, args);
+        System.out.println("welcome to the Address book ");
+    }
+
+}
